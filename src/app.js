5dce2799e6430181d060f7ba48bb53606e9f3b5d
jerome macias espinosa <jerome2103@live.fr>
2019-12-27 14:11:03 +0100

    init app for heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const configureRoutes = require('./routing')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 configureRoutes(app, express);
 
